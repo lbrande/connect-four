6066ff8d5ad5be5c1f71201273a5a24ec8ba9bd1
lbrande <lovbra00@gmail.com>
2020-06-19 22:14:04 +0200

    Renamed Game::is_equals into Game::is_equals_to_cell.

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -42,7 +42,7 @@ impl Game {
                     for d_t in -3..=3 {
                         let i_col = col as i32 + d_t * d_col;
                         let i_row = row as i32 + d_t * d_row;
-                        if self.is_equals(cell, i_col, i_row) {
+                        if self.is_equals_to_cell(i_col, i_row, cell) {
                             count += 1;
                             if count == 4 {
                                 return cell;
@@ -57,7 +57,7 @@ impl Game {
         None
     }
 
-    fn is_equals(&self, cell: Option<Color>, i_col: i32, i_row: i32) -> bool {
+    fn is_equals_to_cell(&self, i_col: i32, i_row: i32, cell: Option<Color>) -> bool {
         (i_col >= 0 && i_col < 7 && i_row >= 0 && i_row < 6)
             && cell == self.board[i_col as usize][i_row as usize]
     }
